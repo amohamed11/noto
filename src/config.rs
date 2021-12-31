a23630cef16c9b9c47005be71374ebff3bb8d870
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2021-12-31 05:38:41 -0700

    feat: base folder defaults to home dir.

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use dirs::home_dir;
 use serde_derive::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -9,9 +10,19 @@ pub struct NotoConfig {
 
 impl ::std::default::Default for NotoConfig {
     fn default() -> Self {
+        // use dirs to find the user's home directory.
+        // if no home dir is found, default to linux tmp dir
+        let folder = {
+            if let Some(home) = home_dir() {
+                home.to_str().unwrap().to_owned() + "/Noto/"
+            } else {
+                "/tmp/Noto/".to_string()
+            }
+        };
+
         Self {
             template: "default".to_string(),
-            base_folder: "/tmp/Noto/".to_string(),
+            base_folder: folder,
             editor: "vim".to_string(),
         }
     }
